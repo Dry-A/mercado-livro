e1351eaf7e885a178906bbcb8ea9b001f114759a
Dry-A <112403510+Dry-A@users.noreply.github.com>
2025-03-10 15:43:00 -0300

    :cat2: Adição da dependencia Flyway

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     kotlin("plugin.jpa") version "1.9.25"
     id("org.springframework.boot") version "3.4.3"
     id("io.spring.dependency-management") version "1.1.7"
+    id("org.flywaydb.flyway") version "9.16.1"
 }
 
 group = "br.com.mercadolivro"
@@ -23,8 +24,13 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    // Banco de Dados
     runtimeOnly("mysql:mysql-connector-java:8.0.33")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    // Flyway
+    implementation("org.flywaydb:flyway-core:10.5.0")
+    implementation("org.flywaydb:flyway-mysql:10.5.0")
+    // Testes
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
