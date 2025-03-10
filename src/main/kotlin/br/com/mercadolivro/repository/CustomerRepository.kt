9bbf1861f202fe29a5b5d3155eb7fba9edb4c7b3
Dry-A <112403510+Dry-A@users.noreply.github.com>
2025-03-10 00:55:09 -0300

    :beetle: Ajustes nos métodos da Interface Repository

--- src/main/kotlin/br/com/mercadolivro/repository/CustomerRepository.kt
+++ src/main/kotlin/br/com/mercadolivro/repository/CustomerRepository.kt
@@ -1,9 +1,7 @@
 package br.com.mercadolivro.repository
 
 import br.com.mercadolivro.model.CustomerModel
-import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
-import org.springframework.data.repository.query.Param
 
 interface CustomerRepository : CrudRepository<CustomerModel, Int> {
 
